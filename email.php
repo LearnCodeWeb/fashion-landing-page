c629867bd6638b1cb467aef8fe487f6104f3f603
Zaid Bin Khalid <MianZaid@users.noreply.github.com>
2017-04-24 11:30:45 +0900

    Add files via upload

---
+++ email.php
@@ -0,0 +1,27 @@
+<?php
+	if(isset($_REQUEST['email']) and $_REQUEST['email']!="")
+	{
+		 $to      	=		$_REQUEST['email']; //"zaid_bin_khalid@hotmail.co.uk"; email sended To
+		 $from		=		"zaid_bin_khalid@hotmail.co.uk"; // Write your email address here. email send From
+		 $name    	=		$_REQUEST['name']; //form Name here
+		 $message   =		$_REQUEST['message']; //form Message here
+		 $subject = "This is subject"; //Your subject here
+		 
+		 $body = "<b>".$to."</b>";
+		 $body .= "<h1>".$name."</h1>";
+		 $body .= "<h1>".$message."</h1>";
+		 
+		 $header = "From:".$from." \r\n";
+		 $header .= "MIME-Version: 1.0\r\n";
+		 $header .= "Content-type: text/html\r\n";
+		 
+		 $retval = mail($to,$subject,$body,$header);
+		 
+		 if($retval==true){
+			echo 1;
+		 }
+		 else{
+			echo 0;
+		 }
+	}
+?>
